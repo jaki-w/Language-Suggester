6e03ed19b3d515cbb9bd529e1b175b5419085ee0
Jaki Widby <jaki.widby@gmail.com>
2019-12-08 15:40:04 -0800

    add css styling, create divs for suggestions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
+  $("#quizSubmit").submit(function(event) {
+    event.preventDefault();
 
-  $("form#name").submit(function(event) {
-    $(".quiz").show();
-    $(".introduction").hide();
+    var petAnswer = $("input:radio[name=pet]:checked").val();
+    var beverageAnswer = $("input: radio[name=beverage]:checked").val();
+    var workAnswer = $("input: radio[name=work]:checked").val();
+    var problemSolvingAnswer = $("input: radio[name=problemSolving]:checked").val();
+    var handednessAnswer = $("input: radio[name=handedness]:checked").val();
 
-  event.preventDefault();
+
+    if (petAnswer === "dogs" && beverageAnswer === "water" || petAnswer === "dogs" && beverageAnswer === "icedTea" || petAnswer === "dogs" && beverageAnswer === "soda") {
+      $("#ruby").show();
+    } else if (petAnswer === "cats" && beverageAnswer === "water" || petAnswer === "cats" && beverageAnswer === "icedTea" || petAnswer === "cats" && beverageAnswer === "soda") {
+      $("javascript").show();
+    } else (petAnswer === "neither" && beverageAnswer === "water" || petAnswer === "neither" && beverageAnswer === "icedTea" || petAnswer === "neither" && beverageAnswer === "soda") {
+      $("python").show();
+    }
 
   });
 });
